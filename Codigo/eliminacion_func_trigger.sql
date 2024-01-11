8f0b66e66e9fe75a9cafae911897fc8a1c013a3a
Aleksei115 <70932379+Aleksei115@users.noreply.github.com>
2024-01-11 02:53:41 -0600

    Delete Codigo directory

--- Codigo/eliminacion_func_trigger.sql
+++
@@ -1,3 +0,0 @@
-DROP FUNCTION verificar_tipo_pago_efectivo();
-
-DROP TRIGGER trg_verificar_tipo_pago_efectivo ON METODO_PAGO;
--- Codigo/eliminacion_indices.sql
+++
@@ -1,6 +0,0 @@
-drop index PRODUCTO_PRECIO;
-drop index CLIENTE_NOMB_APP;
-
-drop index FACTURA_FECHA;
-
-drop index ORDEN_NUM;
